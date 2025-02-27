a567cbf01be1a8e29e21db3e69808317c2352d0b
AmiraAli1612001 <programming.tracks@gmail.com>
2025-02-27 21:06:48 +0200

    df

--- settings.py
+++ settings.py
@@ -1,3 +1,13 @@
+import os
+
+from ecommerce.ecommerce.settings import BASE_DIR
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # تأكد أن المسار صحيح
+
+
 ALLOWED_HOSTS = [
     'web-production-e9b9.up.railway.app',  # أضف نطاق Railway
     'localhost',  # للسماح بالتشغيل المحلي
